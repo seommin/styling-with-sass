ee53996db9c51c54d3574d238ca0f1fb1dee54f8
seommin <seomm1n@naver.com>
2020-12-14 15:06:46 +0900

    #1.Button.js add

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import Button from './Components/Button';
+import './App.scss';
+
+function App() {
+	return (
+		<div className="App">
+			<div className="buttons">
+				<Button size="large">BUTTON</Button>
+				<Button>BUTTON</Button>
+				<Button size="small">BUTTON</Button>
+			</div>
+			<div className="buttons">
+				<Button color="gray" size="large">
+					BUTTON
+				</Button>
+				<Button color="gray">BUTTON</Button>
+				<Button color="gray" size="small">
+					BUTTON
+				</Button>
+			</div>
+			<div className="buttons">
+				<Button color="pink" size="large">
+					BUTTON
+				</Button>
+				<Button color="pink">BUTTON</Button>
+				<Button color="pink" size="small">
+					BUTTON
+				</Button>
+			</div>
+			<div className="buttons">
+				<Button size="large" outline>
+					BUTTON
+				</Button>
+				<Button color="gray" outline>
+					BUTTON
+				</Button>
+				<Button color="pink" size="small" outline>
+					BUTTON
+				</Button>
+			</div>
+			<div className="buttons">
+				<Button size="large" fullWidth>
+					BUTTON
+				</Button>
+				<Button color="gray" size="large" fullWidth>
+					BUTTON
+				</Button>
+				<Button color="pink" size="large" fullWidth>
+					BUTTON
+				</Button>
+			</div>
+		</div>
+	);
+}
+
+export default App;
---
+++ src/Components/Button.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import classNames from 'classnames';
+
+import './Button.scss';
+
+// size: large, medium, small
+function Button({ children, size, color, outline, fullWidth }) {
+	return (
+		<button
+			className={classNames('Button', size, color, {
+				outline,
+				fullWidth,
+			})}
+		>
+			{children}
+		</button>
+	);
+}
+
+Button.defaultProps = {
+	size: 'medium',
+	color: 'blue',
+};
+
+export default Button;
